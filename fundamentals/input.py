276f737b1a66712f3acbb52b4b5e8a55e803ceca
BHAV0207 <bhavyajain4274@gmail.com>
2025-03-03 13:33:37 +0530

    learned about list and strings and inputs

---
+++ fundamentals/input.py
@@ -0,0 +1,10 @@
+print("hello , world")
+
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
+#  in python the input default is srintg so we need to convert it to int for math operations
+number = input("What is your favorite number? ")
+print (int(number) + 6) 
+
+#  in python string are immutable when we try to change the value of a string it will create a new string
---
+++ fundamentals/lists.py
@@ -0,0 +1,18 @@
+l1 =[1,2,3,4,"bhav"]
+# there can be multiple data types in a list
+
+# Accessing elements of a list
+print(l1[0])
+print(l1[4])
+
+# Changing the value of a list
+l1[4] = "bhavesh"
+print(l1)
+
+# Adding elements to a list
+l1.append(5)
+print(l1)
+
+# Removing elements from a list
+l1.remove(5)
+print(l1)
